5578b0cc19cc4755729ff12e7f94c5f3e54ec03c
jiamu chai <jchai9@wisc.edu>
2023-02-05 22:27:31 -0600

    minor tidying up

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,7 +20,6 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.vision.VisionThread;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.apriltag.AprilTagDetector.Config;
@@ -30,6 +29,7 @@ import edu.wpi.first.apriltag.AprilTagDetector.Config;
  * described in the TimedRobot documentation. If you change the name of this class or the package after creating this
  * project, you must also update the build.gradle file in the project.
  */
+@SuppressWarnings("unused")
 public class Robot extends TimedRobot {
 
     public static DriveTrain driveTrain;
