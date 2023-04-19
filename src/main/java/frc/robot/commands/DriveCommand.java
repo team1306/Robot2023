9fe972606e121239440356a0dbde0c588391c8a3
jiamu chai <goatesochai@gmail.com>
2023-04-18 22:19:07 -0500

    outreach turbo removal and speed control

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
+import java.util.List;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.utils.UserAnalog;
 import frc.robot.subsystems.DriveTrain;
@@ -11,6 +13,7 @@ import frc.robot.subsystems.DriveTrain;
 public class DriveCommand extends CommandBase {
     // turbo (no speed limit by default; good movement easily early in game)
     public boolean turbo = true;
+    private static boolean IS_OUTREACH = true;
     private DriveTrain driveTrain;
     private UserAnalog backwardsTurbo;
     private UserAnalog forwardTurbo;
@@ -35,6 +38,9 @@ public class DriveCommand extends CommandBase {
         this.forwardTurbo = forwards;
         this.joystickRotation = rotation;
         this.addRequirements(driveTrain);
+
+        SmartDashboard.putNumber("maxSpd", 0.3);
+        SmartDashboard.putNumber("maxRot", 0.3);
     }
 
     /**
@@ -43,12 +49,20 @@ public class DriveCommand extends CommandBase {
     @Override
     public void execute() {
         // apply deadband to both inputs
+
+        double maxSpd = SmartDashboard.getNumber("maxSpd", 0.3);
+        double maxRot = SmartDashboard.getNumber("maxRot", 0.3);
+
+
+
         double spd = MathUtil.applyDeadband(forwardTurbo.get() - backwardsTurbo.get(), 0.05);
         double rotation = MathUtil.applyDeadband(joystickRotation.get(), 0.05);
 
         // turbo or balancing speed
-        double maxSpeed = turbo ? 1 : 0.3;
-        double maxRotation = turbo ? 1 : 0.3;
+        double maxSpeed = turbo && !IS_OUTREACH ? 1 : maxSpd;
+        double maxRotation = turbo && !IS_OUTREACH ? 1 : maxRot;
+
+        System.out.println(List.of(maxSpeed, maxRotation));
         // maxspeed and maxrotation checks moved into Drivetrain class
         driveTrain.arcadeDrive(spd * maxSpeed, rotation * maxRotation);
     }
